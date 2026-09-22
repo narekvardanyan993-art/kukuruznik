c5c27bce8cfd1b3c04d611288ed2a440ef611f00
Narek Vardanyan <narek.vardanyan993@gmail.com>
2026-09-23 01:16:08 +0400

    feat: lazy load modes, encode videos under 10MB, link index to turntable

--- encode_video.sh
+++ encode_video.sh
@@ -4,8 +4,14 @@ FFMPEG=$(node -e "console.log(require('ffmpeg-static'))")
 for mode in day sunset night; do
   for res in 1x 2x; do
     echo "Encoding $mode $res..."
+    if [ "$res" = "2x" ]; then
+      CRF=30
+    else
+      CRF=23
+    fi
     $FFMPEG -y -framerate 30 -i kukuruznik/turntable/$res/$mode/%03d.webp \
-      -c:v libx264 -preset fast -crf 18 -g 1 -pix_fmt yuv420p -movflags +faststart \
+      -vf "select='not(eq(mod(n\,3)\,2))',setpts=N/FRAME_RATE/TB" \
+      -c:v libx264 -preset veryslow -crf $CRF -g 1 -pix_fmt yuv420p -movflags +faststart \
       kukuruznik/turntable/$res/${mode}.mp4
   done
 done
